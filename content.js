e8b2f335a7719fb424d8ed02e1340544bfd2f1c4
Kassi-Butler <kassi.butler@colorado.edu>
2017-02-28 00:21:10 -0700

    I tried getting hovertext to work but it's giving me issues. Will revisit later

--- content.js
+++ content.js
@@ -1,2 +1,11 @@
 alert("Testing the extension!")
+/*
+var classlinks = document.querySelector('[id^="E_CLASS_NAME"]');
+
+for (var i=0; i<classlinks.length; i++){
+	var link = classlinks[i];
+	console.log(link);
+	link.innerHTML = "Custon Hovertext";
+}
+*/
 
