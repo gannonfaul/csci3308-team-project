0cd2a7391c176437c197c388326862d2ca100734
Scew5145 <scew5145@colorado.edu>
2017-03-13 12:46:53 -0600

    Spit out class names to console
    Makes the loop over your course list spit out the class IDs. We’ll be
    using these to make the queries to the course catalog.

--- script.js
+++ script.js
@@ -8,8 +8,16 @@ $(document).ready(function(){
 	$("iframe")[0].addEventListener("load", function(){
 		iframe = getFrame();
 		iframe.find('.PSLEVEL2GRIDROW').find('span').each(function(i, item){
-			//console.log(item);
 			if(item.id.match("^E_CLASS_NAME")){
+				var textObj = item.firstChild;
+				if(textObj.id != undefined){
+					textObj = textObj.firstChild;
+					console.log(textObj);
+				}
+				else{
+					console.log(textObj);
+				}
+				//console.log(item.firstChild);
 				$(item).attr('title', 'Custom hovertext');
 			}
 			else{
