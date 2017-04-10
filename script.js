f76d779ea087051170053208f1f8d0f078e71d5a
Scew5145 <scew5145@colorado.edu>
2017-04-09 22:04:13 -0600

    Adds Catalog Hyperlinks to Cart
    Comments for major sections added.
    Also, adds a new column to the shopping cart for the hyperlinks to the
    course catalog.
    
    Major Bugs:
    
    Doesn’t update (and disappears) on adding new entries to shopping cart
    
    Link column disappears upon clicking table headers

--- script.js
+++ script.js
@@ -16,8 +16,25 @@ $(document).ready(function(){
 	$("iframe")[0].addEventListener("load", function(){
 		iframe = getFrame();
 		console.log("Load");
+		
+		//Add new header to shopping cart table for course catalog Links
+		tableBody = iframe.find('.PSLEVEL1GRIDNBO');
+		tableBody.width(800) //change the width of the whole table so links fit
+		tableBody = tableBody.find('tbody');
+		secondChild = $(tableBody).children().eq(1);
+		secondChild.append('<th scope="col" width="200" align="left" class="PSLEVEL3GRIDCOLUMNHDR"><a>Course Catalog Link</a></th>')
+		//Currently, Clicking any of the header buttons (select, class, days/times, etc) will remove this header. 
+		//Don't really understand why.
+		//Also, it currently won't update when you add a new item to your shopping cart.
+
+		
 		iframe.find('.PSLEVEL3GRIDWBO').find('span').each(function(i, item){
 			if(item.id.match("^P_CLASS_NAME")){
+				/*
+				First thing's first, after locating a "course" in our shopping cart, we have to get the course name.
+				The text that defines the course ("CSCI 1300", for example) will be found as the first child of item if the 
+				course has no link attached (.id == undefined) and will be the first child's first child if it is a hyperlink.
+				*/
 				var textObj = item.firstChild;
 				console.log(textObj)
 				if(textObj.id != undefined){
@@ -27,9 +44,22 @@ $(document).ready(function(){
 				else{
 					console.log(textObj);
 				}
+				// Split the course name into a department tag and a course number:
 				var classinfo = getNameParts(textObj);
-				//console.log(item.firstChild);
 				$(item).attr('title', classinfo[0]+" "+classinfo[1]);
+				//Create our link to the course catalog based on the info we pulled from the class name:
+				var classCatalogLink = 'http://www.colorado.edu/catalog/2016-17/courses?subject='+classinfo[0]+'&number='+classinfo[1]
+
+				//Now, we have to go to the table row (tr tag) and append it with another element that contains a hyperlink to the
+				//The respective course catalog entry. 
+				//The tr should be the closest one available in the doc tree. Look up 'jquery closest' for more info
+				var tableRow = item.closest('tr');
+				//add a new td after the last entry in the table, to make a new box to put stuff in:
+				$(tableRow).find('td:last').after('<td>'
+				+ '<a href= '+ classCatalogLink+ '>' 
+				+ classinfo[0]+'-'+classinfo[1]
+				+ '</a></td>'); 
+
 				$(item).hover(
 				function() {
 					/*
