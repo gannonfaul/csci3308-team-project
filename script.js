75e6eaf5a13de9f54d14bb2decefef349fc0488d
Scott Ewing <Scew5145@colorado.edu>
2017-05-01 22:23:09 -0600

    re-unbroke things
    yeah

--- script.js
+++ script.js
@@ -555,13 +555,7 @@ $(document).ready(function(){
 						courseDict[course]["days"] = schedule[0];
 						courseDict[course]["time"] = schedule[1];
 						courseDict[course]["times"] = schedule[2];
-						var timeSpan = (schedule[2][1]-schedule[2][0]) / 100;
-						if (timeSpan % 1 == 0) {
-							courseDict[course]["span"] = timeSpan * 2;
-						} else {
-							timeSpan = Math.ceil(timeSpan) + 1;
-							courseDict[course]["span"] = timeSpan;
-						}
+						courseDict[course]["span"] = Math.ceil((schedule[2][1]-schedule[2][0])/50);
 						break;
 					}
 				}
@@ -670,7 +664,7 @@ $(document).ready(function(){
 				$(this).closest('tr').find('div').each(function(i, imgItem){
 					if(imgItem.id.match("^win0divDERIVED_REGFRM1_SSR_STATUS_LONG")){
 						if($(this).find('img').attr('alt') == "Dropped") {
-							console.log(fullCourseName + " has been dropped.");
+							console.log("This class has been dropped");
 							courseDict[fullCourseName]["dropped"] = true;
 						}
 					}
@@ -695,13 +689,7 @@ $(document).ready(function(){
 						courseDict[course]["days"] = schedule[0];
 						courseDict[course]["time"] = schedule[1];
 						courseDict[course]["times"] = schedule[2];
-						var timeSpan = (schedule[2][1]-schedule[2][0]) / 100;
-						if (timeSpan % 1 == 0) {
-							courseDict[course]["span"] = timeSpan * 2;
-						} else {
-							timeSpan = Math.floor(timeSpan) * 2 + 1;
-							courseDict[course]["span"] = timeSpan;
-						}
+						courseDict[course]["span"] = Math.ceil((schedule[2][1]-schedule[2][0])/50);
 						break;
 					}
 				}
@@ -805,14 +793,14 @@ $(document).ready(function(){
 		var weekdays = ["Mo", "Tu", "We", "Th", "Fr"];
 		var conflictDict = {};
 		var time = 800;
-		var half = false;
+		var half = true;
 		var time_add;
 		var overflowStr = ''
-		var overflowRows = [[0, ''],[0, ''],[0, ''],[0, ''],[0, '']] //For dealing with multi-row crap. 
+		var overflowRows = [[0,''],[0,''],[0,''],[0,''],[0,'']] //For dealing with multi-row crap. 
 		while(time<=max_time){
-			var civ_time = String(Math.floor(time/100));
+			var civ_time = String(Math.ceil(time/100));
 			if (time<1200){
-				if(half == false){
+				if(half == true){
 					civ_time += ":00 am"
 				}
 				else{
@@ -820,30 +808,24 @@ $(document).ready(function(){
 				}
 			}
 			else if (time>1200){
-				if(half == false){
-					civ_time = String(Math.floor((time/100)-12))+":00 pm"
+				if(half == true){
+					civ_time = String(Math.ceil((time/100)-12))+":00 pm"
 				}
 				else{
-					if(time > 1230){
-						civ_time = String(Math.floor((time/100)-12))+":30 pm"
-					} else {
-						civ_time = String(Math.floor(time/100)) + ":30 pm"
-					}
+					civ_time = String(Math.ceil((time/100)-12))+":30 pm"
 				}
 			}
 			else{
 				civ_time += ":00 pm"
 			}
-			// console.log("Time: " + time + " " + "Civ Time: " + civ_time);
-
-			if (half == false){
+			if (half == true){
 				time_add = 30;
 			}
 			else{
 				time_add = 70;
 			}
 			for (j = 0; j < 5; j++){ //
-				if(overflowRows[j] > 0){
+				if(overflowRows[j][0] > 0){
 					overflowStr += weekdays[j]
 				}
 			}
@@ -861,27 +843,37 @@ $(document).ready(function(){
 							if (courseDict[course]["dropped"] == false){
 								if(empty == true && overflowRows[i][0] == 0){
 									overflowRows[i][0] = courseDict[course]["span"]
-									overflowRows[i][1] = course
 									console.log(course, 'overflows by ' + courseDict[course]["span"])
 									calendar += "<td class='SSSWEEKLYBACKGROUND' rowspan='"+String(courseDict[course]["span"])+"'>"
 									calendar += "<span class='SSSTEXTWEEKLY' >"+course+"<br>"+courseDict[course]["instr"]+"<br>"+courseDict[course]["time"]+"<br>"+courseDict[course]["location"]+"<br>"+courseDict[course]["units"]+"</span></td>"
 									empty = false
 									prevEntry = course
 								}else{
+									var conTime = time + (half*50)
 									if(empty == false){ // Conflict where two courses start at the same time
-										conflictDict[i + ' ' + time] = [course,prevEntry]
+										if(conflictDict[i, + ' ' + conTime] == undefined){
+											conflictDict[i + ' ' + conTime] = [prevEntry, course]
+										}
+										else{
+											conflictDict[i, + ' ' + conTime] = conflictDict[i, + ' ' + conTime].concat(course)
+										}
 									} else { // Conflict where the tail of one class overlaps the other
-										console.log("I'm crying rn", course)
-										if(conflictDict[i + ' ' + time] == undefined){
-											conflictDict[i + ' ' + time] = [overflowRows[i][1],course]
-										}else{
-											conflictDict[i + ' ' + time] = conflictDict[i + ' ' + time].concat(course)
+										if(conflictDict[i, + ' ' + conTime] == undefined){
+											console.log('new conflict entry for ' + course + ' and ' + overflowRows[i][1])
+											conflictDict[i, + ' ' + conTime] = [overflowRows[i][1], course]
+										} else {
+											conflictDict[i, + ' ' + conTime] = conflictDict[i, + ' ' + conTime].concat(course)	
 										}
 									}
 								}
 							}
 						}
-
+						
+						//THIS IS WHERE SHIT IS BREAKING!!!
+						/*else if((courseDict[course]["times"][0] < time)&&(courseDict[course]["times"][0]>= time-(50*courseDict[course]["span"]))){
+							empty = false;
+						}*/
+						//
 					}
 				}
 				if (empty == true){
@@ -898,14 +890,8 @@ $(document).ready(function(){
 			}
 			console.log(overflowRows)
 			calendar += "</tr>"
-			if (half == false){
-				time += time_add;
-				half = true;
-			}
-			else{
-				time += time_add;
-				half = false;
-			}
+			time += time_add;
+			half = !half;
 			overflowStr = ''
 		}
 		console.log(conflictDict)
@@ -919,7 +905,7 @@ $(document).ready(function(){
 		var calendar = iframe.find('#SHOPPING_CART_SCHED_HTMLAREA');
 
 		calendar.find('tr').each(function(i, row){
-			rowTime = i*100 +700
+			rowTime = i*50 +700
 			for (conflict in conflictDict){
 				if (conflict.substr(2, conflict.length)  == rowTime) {
 					var dayIndex = parseInt(conflict[0]) + 1
