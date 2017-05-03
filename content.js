b4b5f8e3ebabd13785bb2f4b1f94955f8066534b
kassi-butler <kassi.butler@colorado.edu>
2017-05-03 15:49:03 -0600

    I give up.

--- content.js
+++ content.js
@@ -1,22 +1,10 @@
 //alert("Testing the extension!");
 console.log("content.js initiated");
 
-
-var d = document.createElement('script');
-d.src = chrome.extension.getURL('dictionary.js');
-d.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(d);
-console.log("dictionary added");
-
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 s.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(s);
-console.log("script added");	
-
-d.onload = function(){
-	d.parentNode.removeChild(d);
-}
+document.getElementsByTagName('head')[0].appendChild(s);	
 
 s.onload = function(){
 	s.parentNode.removeChild(s);
