1e6562c1ca96c3109182c73b6a09f4f0e67141b7
kassi-butler <kassi.butler@colorado.edu>
2017-05-03 15:32:59 -0600

    Put it mostly back together

--- content.js
+++ content.js
@@ -1,11 +1,22 @@
 //alert("Testing the extension!");
 console.log("content.js initiated");
 
+
+var d = document.createElement('script');
+d.src = chrome.extension.getURL('dictionary.js');
+d.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(d);
+console.log("dictionary added");
+
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('script.js');
 s.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(s);
 console.log("script added");	
+
+d.onload = function(){
+	d.parentNode.removeChild(d);
+}
 
 s.onload = function(){
 	s.parentNode.removeChild(s);
